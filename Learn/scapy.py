a9b9be35a482daca7abc72cfe4d7ab6312634864
SecureUniverse <64855572+SecureUniverse@users.noreply.github.com>
2021-10-30 12:57:26 +0330

    Create scapy.py

---
+++ Learn/scapy.py
@@ -0,0 +1,12 @@
+
+#---------------------------  Download & Install  ---------------------------
+"https://github.com/secdev/scapy/releases
+
+> python setup.py install"
+> scapy
+> pip install --pre scapy[basic]
+
+#---------------------------  Commands  ---------------------------
+
+
+#---------------------------  Scapy in Python  ---------------------------
