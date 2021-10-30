161ff4925a573971fc826234cd63bd9881fe0dbe
SecureUniverse <64855572+SecureUniverse@users.noreply.github.com>
2021-10-30 12:21:07 +0330

    Update file.py

--- Learn/file.py
+++ Learn/file.py
@@ -1,10 +1,10 @@
 #---------------------------  Iteration  ---------------------------
-f = open(‘/tmp/scores.txt’)
+f = open('/tmp/scores.txt')
 for line in f:
     print(line.strip())
     
 #---------------------------  read  ---------------------------
-f = open(‘/tmp/scores.txt’)
+f = open('/tmp/scores.txt')
 lines = f.readlines()
 lines
 #[‘12\n’, ‘9\n’, ‘8\n’, ‘1\8n’, ‘20\n’, ‘11\n’, ‘3\n’, ‘3.75\n’, ‘9\n’, ‘9\n’]
@@ -20,10 +20,10 @@ f.close()
 #---------------------------  CSV  ---------------------------
 import csv
 from statistics import mean
-with open(‘/tmp/grades.csv’, ‘rb’) as f:
+with open('/tmp/grades.csv', 'rb') as f:
     reader = csv.reader(f)
     for row in reader:
         name = row[0]
         for grade in row[1:]:
             these_grades.append(int(grade))
-        print(“average of %10s is %6f” % (name, mean(these_grades0))
+        print("average of %10s is %6f" % (name, mean(these_grades0))
